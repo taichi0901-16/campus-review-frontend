1c0ba69371f57b8cb010a7107b331e53d7f38cc1
taichi0901-16 <b.taichi0901@icloud.com>
2025-05-12 21:58:52 +0900

    データベース接続変更

--- src/components/Home/ReviewUpdate.js
+++ src/components/Home/ReviewUpdate.js
@@ -30,6 +30,7 @@ function ReviewEditForm() {
       try {
         const res = await axios.get(`${API_URL}/plane_review/${id}`);
         setReviewDetails(res.data);
+        console.log("プレーンデータ",res.data)
         const defaultValues = {
           ...res.data,
           selected_features: res.data.feature_ids.split(","),
