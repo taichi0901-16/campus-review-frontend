448fb1dcbce62b3fc4654b374735e94c9d1de643
taichi0901-16 <b.taichi0901@icloud.com>
2025-05-12 22:28:29 +0900

    データベース接続変更

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/">
     <App />
     </Router>
   </React.StrictMode>
